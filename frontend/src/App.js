02b3408a89711129aac4ab0c893da5e5b756b73b
vkrajput26 <indvivekrajput@gmail.com>
2023-01-07 23:19:33 +0530

    adding chakra ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+
 import Navbar from './Components/Navbar';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{}}>
+      <Box  backgroundImage="url('https://images.unsplash.com/photo-1519419166318-4f5c601b8e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')">
+          
+      </Box>
      <Navbar/>
     </div>
   );
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div>
       <img src="" alt="" />
-     
+     asd
     </div>
   )
 }
