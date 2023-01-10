57a4b2452c250da7a2939b92994144ac5a49a73f
vkrajput26 <indvivekrajput@gmail.com>
2023-01-10 23:31:33 +0530

    changes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 
 import Navbar from './Components/Navbar';
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 
 function App() {
   return (
     <div className="App" style={{}}>
-      <Box  backgroundImage="url('https://images.unsplash.com/photo-1519419166318-4f5c601b8e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')">
+      <Box >
           
-      </Box>
      <Navbar/>
+     <Image src="https://images.unsplash.com/photo-1519419166318-4f5c601b8e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+      </Box>
     </div>
   );
 }
